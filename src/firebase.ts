d70e4b92ee772045fdacc543daf73553a21d7f9a
Carlos Adrián Delgado Vázquez <136146548+cadva02@users.noreply.github.com>
2025-06-23 16:22:26 -0600

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsc-qeTyEU7Uyzh8Isedv_gbEuoX3wq2M",
-  authDomain: "gaid-agent.firebaseapp.com",
-  projectId: "gaid-agent",
-  storageBucket: "gaid-agent.firebasestorage.app",
-  messagingSenderId: "175989187361",
-  appId: "1:175989187361:web:7844d3f9df86094d08d460",
-  measurementId: "G-1ZM6KXMNWQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
